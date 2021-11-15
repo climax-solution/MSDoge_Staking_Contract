88b288a402eae2e6f58379f2e34e157a40210f98
climax-solution <blocklimax@gmail.com>
2021-11-16 04:25:55 +0900

    update staking smart contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -10,14 +10,10 @@ contract Staking {
     uint public rewardRate;
     uint public lastUpdateTime;
     uint public rewardPerTokenStored;
-
-    mapping(address => uint) public userRewardPerTokenPaid;
-    mapping(address => uint) public rewards;
-    mapping(address => uint) private _rates;
-    
     uint private _totalSupply;
-    mapping(address => uint) private _balances;
-    mapping(address => uint256) private _stakedTime;
+    uint256 private _totalStakedUserCount;
+    address[] private _stakedAddressList;
+
     
     struct StakingItem {
         uint _balance;
@@ -26,10 +22,7 @@ contract Staking {
         uint _rate;
         bool isExisted;
     }
-    
-    address[] private _stakedAddressList;
-    uint256 private _totalStakedUserCount;
-    mapping(address => StakingItem) private _stakingList;
+    mapping(address => StakingItem[]) private _stakingList;
     
     constructor(address _stakingToken, address _rewardsToken, uint _rewardRate) {
         stakingToken = IERC20(_stakingToken);
@@ -38,44 +31,55 @@ contract Staking {
     }
 
     modifier _isExistedAccount(address account) {
-        bool _isExisted = false; uint flag;
-        for (uint i = 0; i < _stakedAddressList.length; i ++) {
-            if (_stakedAddressList[i] == account) {
-                _isExisted = true; flag = i;
-            }
-        }
-        if (_isExisted) {
-            delete _stakedAddressList[flag];
-        }
+        // bool _isExisted = false; uint flag;
+        // for (uint i = 0; i < _stakedAddressList.length; i ++) {
+        //     if (_stakedAddressList[i] == account) {
+        //         _isExisted = true; flag = i;
+        //     }
+        // }
+        // if (_isExisted) {
+        //     delete _stakedAddressList[flag];
+        // }
         _;
     }
     
     function stake(uint _amount) external _isExistedAccount(msg.sender){
         _totalSupply += _amount;
         _stakedAddressList.push(msg.sender);
-        _stakingList[msg.sender]._balance += _amount;
-        _stakingList[msg.sender]._rate = rewardRate;
-        if (!_stakingList[msg.sender].isExisted) _stakingList[msg.sender]._created_at = block.timestamp;
-        _stakingList[msg.sender]._updated_at = block.timestamp;
+        if (_stakingList[msg.sender].length > 0) {
+            uint lastIdx = _stakingList[msg.sender].length - 1;
+            if (_stakingList[msg.sender][lastIdx]._rate == rewardRate) {
+                _stakingList[msg.sender][lastIdx]._balance += _amount;
+                _stakingList[msg.sender][lastIdx]._created_at = block.timestamp;
+            }
+            else {
+                _stakingList[msg.sender].push(StakingItem(_amount, block.timestamp, block.timestamp, rewardRate, true));
+            }
+        }
+        else {
+            _stakingList[msg.sender].push(StakingItem(_amount, block.timestamp, block.timestamp, rewardRate, true));
+        }
         rewardsToken.transfer(msg.sender, _amount);
     }
 
-    function withdraw() external {
-        _totalSupply -= _stakingList[msg.sender]._balance;
-        uint _amount = _stakingList[msg.sender]._balance;
-        _stakingList[msg.sender]._balance = 0;
+    function withdraw(uint idx) external {
+        _totalSupply -= _stakingList[msg.sender][idx]._balance;
+        uint _amount = _stakingList[msg.sender][idx]._balance;
+        _stakingList[msg.sender][idx]._balance = 0;
         
-        if ( block.timestamp > _stakingList[msg.sender]._created_at + 15 days) {
-            stakingToken.transfer(msg.sender, _amount);            
+        if ( block.timestamp > _stakingList[msg.sender][idx]._created_at + 15 days) {
+            stakingToken.transfer(msg.sender, _amount);
+            delete _stakingList[msg.sender][idx];
         }
 
+        
     }
     
-    function claim() public {
-        if ( block.timestamp > _stakingList[msg.sender]._updated_at + 15 days) {
-            uint _reward = _stakingList[msg.sender]._balance * _stakingList[msg.sender]._rate / 100;
+    function claim(uint idx) public {
+        if ( block.timestamp > _stakingList[msg.sender][idx]._updated_at + 15 days) {
+            uint _reward = _stakingList[msg.sender][idx]._balance * _stakingList[msg.sender][idx]._rate / 100;
             stakingToken.transfer(msg.sender, _reward);
-            _stakingList[msg.sender]._updated_at = block.timestamp;
+            _stakingList[msg.sender][idx]._updated_at = block.timestamp;
         }
     }
     
